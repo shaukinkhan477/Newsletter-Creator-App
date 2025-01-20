fad6df5e42332b21f8015e6e6c67560a328f8ec2
shaukinkhan477 <shaukinkhan477@gmail.com>
2025-01-20 16:09:52 +0530

    Enhanced UI

--- newsLetterCreatorApp/src/app/components/sidebar/sidebar.component.ts
+++ newsLetterCreatorApp/src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonEngine } from '@angular/ssr';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  @Output() toggleSidebar = new EventEmitter<boolean>();
+  isCollapsed = false; // Sidebar is expanded by default
+
+  toggle() {
+    this.isCollapsed = !this.isCollapsed;
+    this.toggleSidebar.emit(this.isCollapsed);
+  }
+}
